5f9267dffa54343f309dd6edfa4a1b8718a25699
Saber-grou <111590089+Kevin-Locatelli@users.noreply.github.com>
2023-02-14 10:24:54 +0100

    Login

--- login.js
+++ login.js
@@ -2,26 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/login', (req, res) => {
-<<<<<<< Updated upstream
-    res.send('<form method="POST" action="/login">' +
-             '<label for="username">Username:</label>' +
-             '<input type="text" id="username" name="username">' +
-             '<br>' +
-             '<label for="password">Password:</label>' +
-             '<input type="password" id="password" name="password">' +
-             '<br><br>' +
-             '<input type="submit" value="Submit">' +
-             '</form>');
-});
-
-app.post('/login', (req, res) => {
-    // Handle form submission here
-});
-              
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-});
-=======
     res.sendFile(__dirname + '/login.html');
 });
 
@@ -33,4 +13,4 @@ app.get('/style.css', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000/login');
 });
->>>>>>> Stashed changes
+
