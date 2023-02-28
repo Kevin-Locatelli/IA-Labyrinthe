58194200d9f0a22adc679322f381a0a10a7da74b
Saber-grou <111590089+Kevin-Locatelli@users.noreply.github.com>
2023-02-28 10:12:19 +0100

    test commit

--- game.js
+++ game.js
@@ -1,92 +1,74 @@
-// Récupérer les éléments de l'interface utilisateur
-const createGridBtn = document.getElementById("createGridBtn");
-const findPathBtn = document.getElementById("findPathBtn");
-const gridSizeInput = document.getElementById("gridSize");
-const startXInput = document.getElementById("startX");
-const startYInput = document.getElementById("startY");
-const endXInput = document.getElementById("endX");
-const endYInput = document.getElementById("endY");
-const pathfindingResultsDiv = document.getElementById("pathfindingResults");
+/*window.onload = () => {
+    const grid = document.getElementById("grid-container");
+    const createGridBtn = document.getElementById("create-grid-btn");
+    const findPathBtn = document.getElementById("find-path-btn");
+    let gridCells = [];
 
-let grid = null;
+    function createGrid() {
+        const width = 10;
+        const height = 10;
+        const gridHtml = [];
 
-// Fonction pour créer une grille vide
-function createEmptyGrid(size) {
-    const emptyGrid = [];
-    for (let i = 0; i < size; i++) {
-        emptyGrid.push([]);
-        for (let j = 0; j < size; j++) {
-            emptyGrid[i].push(0);
+        for (let i = 0; i < height; i++) {
+            const rowHtml = [];
+            for (let j = 0; j < width; j++) {
+                rowHtml.push(`<div class="cell" data-row="${i}" data-col="${j}"></div>`);
+            }
+            gridHtml.push(`<div class="row">${rowHtml.join("")}</div>`);
         }
-    }
-    return emptyGrid;
-}
 
-// Fonction pour dessiner la grille sur la page
-function drawGrid() {
-    let gridHtml = "";
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === 0) {
-                gridHtml += `<div class="grid-cell empty"></div>`;
-            } else if (grid[i][j] === 1) {
-                gridHtml += `<div class="grid-cell obstacle"></div>`;
-            } else if (grid[i][j] === 2) {
-                gridHtml += `<div class="grid-cell start"></div>`;
-            } else if (grid[i][j] === 3) {
-                gridHtml += `<div class="grid-cell end"></div>`;
+        grid.innerHTML = gridHtml.join("");
+        gridCells = document.querySelectorAll(".cell");
+    }
+
+    function findPath() {
+        // Créer une grille 2D représentant l'état de chaque cellule de la grille
+        const matrix = [];
+        for (let i = 0; i < 10; i++) {
+            const row = [];
+            for (let j = 0; j < 10; j++) {
+                if (gridCells[i * 10 + j].classList.contains("obstacle")) {
+                    row.push(1);
+                } else {
+                    row.push(0);
+                }
+            }
+            matrix.push(row);
+        }
+
+        // Trouver le chemin le plus court
+        const finder = new PF.AStarFinder();
+        const start = [0, 0];
+        const end = [9, 9];
+        const path = finder.findPath(start[0], start[1], end[0], end[1], matrix);
+
+        // Dessiner le chemin sur la grille
+        for (const cell of gridCells) {
+            const row = parseInt(cell.getAttribute("data-row"));
+            const col = parseInt(cell.getAttribute("data-col"));
+            cell.classList.remove("path");
+            if (path.some((p) => p[0] === row && p[1] === col)) {
+                cell.classList.add("path");
             }
         }
     }
-    document.getElementById("grid").innerHTML = gridHtml;
-}
 
-// Fonction pour initialiser la grille
-function createGrid() {
-    const size = parseInt(gridSizeInput.value);
-    grid = createEmptyGrid(size);
-    grid[parseInt(startXInput.value) - 1][parseInt(startYInput.value) - 1] = 2;
-    grid[parseInt(endXInput.value) - 1][parseInt(endYInput.value) - 1] = 3;
-    drawGrid();
-}
+    // Ajouter des écouteurs d'événements pour les boutons
+    createGridBtn.addEventListener("click", createGrid);
+    findPathBtn.addEventListener("click", findPath);
 
-// Fonction pour ajouter ou retirer un obstacle dans la grille
-function toggleObstacle(event) {
-    const cell = event.target;
-    const row = cell.dataset.row;
-    const col = cell.dataset.col;
-    if (grid[row][col] === 0) {
-        grid[row][col] = 1;
-        cell.classList.add("obstacle");
-    } else {
-        grid[row][col] = 0;
-        cell.classList.remove("obstacle");
-    }
-}
+    // Créer la grille initiale
+    createGrid();
+};
 
-// Fonction pour trouver le chemin le plus court entre le point de départ et le point d'arrivée
-function findPath() {
-    const finder = new PF.AStarFinder();
-    const start = [parseInt(startXInput.value) - 1, parseInt(startYInput.value) - 1];
-    const end = [parseInt(endXInput.value) - 1, parseInt(endYInput.value) - 1];
-    const path = finder.findPath(start[0], start[1], end[0], end[1], new PF.Grid(grid));
-    pathfindingResultsDiv.innerHTML = path.length === 0 ? "No path found" : `Path found with ${path.length} steps`;
-// Dessiner le chemin sur la grille
-    const gridCells = document.querySelectorAll(".grid-cell");
-    for (const cell of gridCells) {
-        const row = cell.dataset.row;
-        const col = cell.dataset.col;
-        if (path.some(step => step[0] == row && step[1] == col)) {
-            cell.classList.add("path");
-        } else {
-            cell.classList.remove("path");
-        }
-    }
-}
+const apiUrl = 'http://localhost:3000';
 
-// Ajouter les gestionnaires d'événements pour les boutons
-createGridBtn.addEventListener("click", createGrid);
-findPathBtn.addEventListener("click", findPath);
+fetch(apiUrl)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('#app').innerHTML = data;
+    })
+    .catch(error => {
+        console.error(error);
+    });*/
 
-// Générer la grille initiale
-createGrid();
