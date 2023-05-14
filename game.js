edca54766aa9ba1d94e33f909ca0debf32067693
Saber-grou <111590089+Kevin-Locatelli@users.noreply.github.com>
2023-05-14 15:48:01 +0200

    jeu qui fonctionne

--- game.js
+++ game.js
@@ -3,9 +3,18 @@ document.createSvg = function(tagName) {
     return this.createElementNS(svgNS, tagName);
 };
 
-const numberPerSide = 50;
+const numberPerSide = 20;
 const size = 9;
 const pixelsPerSide = 900;
+let tab = new Array(numberPerSide);
+
+let depart = 0;
+let departX = 0;
+let departY = 0;
+
+let arrive = 0;
+let arriveX = 0;
+let arriveY = 0;
 
 
 
@@ -16,110 +25,107 @@ let grid = function(numberPerSide, size, pixelsPerSide, colors) {
     svg.setAttribute("viewBox", [0, 0, numberPerSide * size, numberPerSide * size].join(" "));
 
     for(let i = 0; i < numberPerSide; i++) {
+        tab[i] = new Array(numberPerSide);
+
         for(let j = 0; j < numberPerSide; j++) {
+            tab[i][j] = 0;
             let color1 = colors[(i+j) % colors.length];
-            let color2 = colors[(i+j+1) % colors.length];
             let g = document.createSvg("g");
             g.setAttribute("transform", ["translate(", i*size, ",", j*size, ")"].join(""));
-            let number = numberPerSide * i + j;
+            let numberX = j;
+            let numberY = i;
             let box = document.createSvg("rect");
             box.setAttribute("width", size);
             box.setAttribute("height", size);
             box.setAttribute("fill", color1);
-            box.setAttribute("id", "b" + number);
+            box.setAttribute("id",  (numberY > 9 ? numberY.toString() : numberY + '-') + (numberX > 9 ? numberX.toString() : numberX + '-'));
             g.appendChild(box);
             svg.appendChild(g);
         }
+
     }
+
     svg.addEventListener(
         "click",
         function(e){
-            let id = e.target.id ? document.getElementById(e.target.id) : null
-            if(id)
-                id.setAttribute("fill", "blue");
+            let Tcase = e.target.id ? document.getElementById(e.target.id) : null;
+            let numberY = Tcase.id.slice(0, 1);
+            if(Tcase.id.slice(1, 2) != '-')
+                numberY = Tcase.id.slice(0, 2);
+
+            let numberX = Tcase.id.slice(2, 3);
+            if (Tcase.id.slice(3, 4) != '-')
+                numberX = Tcase.id.slice(2, 4);
+
+            if(depart == 0 && arrive == 0) {
+                if (Tcase && Tcase.getAttribute("fill") != "blue") {
+                    Tcase.setAttribute("fill", "blue");
+                    tab[numberY][numberX] = 1;
+                } else {
+                    if (Tcase) {
+                        Tcase.setAttribute("fill", "white");
+
+                        tab[numberY][numberX] = 0;
+                    }
+                }
+            }
+            else if(depart == 1) {
+                Tcase.setAttribute("fill", "green");
+                console.log(Tcase.id)
+                console.log(numberY, numberX);
+                tab[numberY][numberX] = 0;
+                departX = numberX;
+                departY = numberY;
+
+                depart = 0;
+            }
+            else {
+                Tcase.setAttribute("fill", "red");
+                tab[numberY][numberX] = 0;
+                arriveX = numberX;
+                arriveY = numberY;
+
+                arrive = 0;
+            }
         },
         false);
     return svg;
 };
 
 let container = document.getElementById("container");
-container.appendChild(grid(numberPerSide, size, pixelsPerSide, ["black", "white"]));
+container.appendChild(grid(numberPerSide, size, pixelsPerSide, ["white"]));
 
+document.getElementById("depart").addEventListener("click", function() {
+    depart = 1;
 
+});
 
-/*
+document.getElementById("arriver").addEventListener("click", function() {
+    arrive = 1;
 
-// Créer une carte 2D avec des obstacles
+});
+document.getElementById("start").addEventListener("click", function() {
 
+    const grid = new PF.Grid(tab);
+    const finder = new PF.AStarFinder();
+    console.log(departY, departX, arriveY, arriveX);
+    const path = finder.findPath(departX, departY, arriveX, arriveY, grid);
+    console.log(path);
+    for(let i = 1; i < path.length-1; i++) {
+        let id;
+        if(path[i][1] < 10)
+            id = path[i][1] + '-';
+        else
+            id = path[i][1].toString();
+        if(path[i][0] < 10)
+            id += path[i][0] + '-';
+        else
+            id += path[i][0].toString();
 
-let map = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-// Créer un objet "grid" à partir de la carte
-const grid = new PF.Grid(map);
-
-// Trouver le chemin le plus court entre deux points
-const finder = new PF.AStarFinder();
-const path = finder.findPath(0, 0, 3, 3, grid);
-
-// Afficher le chemin sur un canvas
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const cellSize = 900 / map.length;
-
-for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-        ctx.beginPath();
-        ctx.rect(j * cellSize, i * cellSize, cellSize, cellSize);
-        ctx.fillStyle = map[i][j] === 1 ? 'black' : 'white';
-        ctx.fill();
-        ctx.strokeStyle = 'gray';
-        ctx.stroke();
+        document.getElementById(id).setAttribute("fill", "yellow");
     }
-}
+});
 
-for (let i = 0; i < path.length; i++) {
-    const x = path[i][0] * cellSize + cellSize / 2;
-    const y = path[i][1] * cellSize + cellSize / 2;
-
-    if (i === 0) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);
-    }
-}
-// Ajouter un événement onclick à chaque case
-for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-        createElementOnDom('canvas',`cell-${i}-${j}`);
-        const cell = document.getElementById('cell-${i}-${j}');
-        console.log(cell);
-        if (cell !== null) {
-            cell.addEventListener('click', function () {
-                // Cette fonction sera appelée lorsque l'utilisateur clique sur la case
-                console.log(`Cell clicked: ${i}, ${j}`);
-            });
-        }
-    }
-}
-
-ctx.strokeStyle = 'green';
-ctx.stroke();
-
-
-function createElementOnDom (type,id) {
-    let element=document.createElement(type);
-    element.id=id;
-    return element;
-}
-*/
+document.getElementById("reset").addEventListener("click", function() {
+    window.location.reload();
+});
